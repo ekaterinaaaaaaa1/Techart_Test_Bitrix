93951e83257cc65d1f331ceb71d92acffe883eac
ekaterinaaaaaaa1 <ekaterina_kuzmina_2002@mail.ru>
2025-09-12 10:55:13 +0300

    Удалено лишнее из файлов шаблона catalog/countries.

--- components/bitrix/catalog/countries/sections.php
+++ components/bitrix/catalog/countries/sections.php
@@ -16,58 +16,6 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 $this->setFrameMode(true);
 $this->addExternalCss("/bitrix/css/main/bootstrap.css");
 
-$sectionListParams = array(
-	"IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
-	"IBLOCK_ID" => $arParams["IBLOCK_ID"],
-	"CACHE_TYPE" => $arParams["CACHE_TYPE"],
-	"CACHE_TIME" => $arParams["CACHE_TIME"],
-	"CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
-	"COUNT_ELEMENTS" => $arParams["SECTION_COUNT_ELEMENTS"],
-	"TOP_DEPTH" => $arParams["SECTION_TOP_DEPTH"],
-	"SECTION_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"],
-	"VIEW_MODE" => $arParams["SECTIONS_VIEW_MODE"],
-	"SHOW_PARENT_NAME" => $arParams["SECTIONS_SHOW_PARENT_NAME"],
-	"HIDE_SECTION_NAME" => ($arParams["SECTIONS_HIDE_SECTION_NAME"] ?? "N"),
-	"ADD_SECTIONS_CHAIN" => ($arParams["ADD_SECTIONS_CHAIN"] ?? '')
-);
-if ($sectionListParams["COUNT_ELEMENTS"] === "Y")
-{
-	$sectionListParams["COUNT_ELEMENTS_FILTER"] = "CNT_ACTIVE";
-	if ($arParams["HIDE_NOT_AVAILABLE"] == "Y")
-	{
-		$sectionListParams["COUNT_ELEMENTS_FILTER"] = "CNT_AVAILABLE";
-	}
-}
-$APPLICATION->IncludeComponent(
-	"bitrix:catalog.section.list",
-	"",
-	$sectionListParams,
-	$component,
-	($arParams["SHOW_TOP_ELEMENTS"] !== "N" ? array("HIDE_ICONS" => "Y") : array())
-);
-unset($sectionListParams);
-
-if ($arParams["USE_COMPARE"] === "Y")
-{
-	$APPLICATION->IncludeComponent(
-		"bitrix:catalog.compare.list",
-		"",
-		array(
-			"IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
-			"IBLOCK_ID" => $arParams["IBLOCK_ID"],
-			"NAME" => $arParams["COMPARE_NAME"],
-			"DETAIL_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["element"],
-			"COMPARE_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["compare"],
-			"ACTION_VARIABLE" => (!empty($arParams["ACTION_VARIABLE"]) ? $arParams["ACTION_VARIABLE"] : "action"),
-			"PRODUCT_ID_VARIABLE" => $arParams["PRODUCT_ID_VARIABLE"],
-			'POSITION_FIXED' => $arParams['COMPARE_POSITION_FIXED'] ?? '',
-			'POSITION' => $arParams['COMPARE_POSITION'] ?? ''
-		),
-		$component,
-		array("HIDE_ICONS" => "Y")
-	);
-}
-
 if ($arParams["SHOW_TOP_ELEMENTS"] !== "N")
 {
 	if (isset($arParams['USE_COMMON_SETTINGS_BASKET_POPUP']) && $arParams['USE_COMMON_SETTINGS_BASKET_POPUP'] === 'Y')
