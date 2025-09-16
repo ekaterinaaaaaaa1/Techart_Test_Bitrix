f1fe79c55bccdafbf3d169fb74cabdeac94a1aef
ekaterinaaaaaaa1 <ekaterina_kuzmina_2002@mail.ru>
2025-09-16 09:07:22 +0300

    На главной карточки стоят в один столбец, на детальной не работает лупа.

--- frontend/src/block/common/catalog-item/catalog-item.blade.php
+++ frontend/src/block/common/catalog-item/catalog-item.blade.php
@@ -1,10 +1,11 @@
 <div class="{{ $block }} product-item">
 	<a class="{{ $block->elem('link') }}" href="{!! $item['DETAIL_PAGE_URL'] !!}" data-entity="image-wrapper">
-		<img class="{{ $block->elem('img') }} product-item-image-original" id="{{ $itemIds['PICT'] }}" src="{{ $item['DETAIL_PICTURE']['SRC'] }}" alt=""></img>
-        <img class="{{ $block->elem('img') }} product-item-image-alternative" id="{{ $itemIds['SECOND_PICT'] }}" src="{{ $item['DETAIL_PICTURE']['SRC'] }}" alt=""></img>
-        <p class="{{ $block->elem('name') }} product-item-title">{!! $item['NAME'] !!}</p>
-		@if (!empty($arResult['COUNTRIES_STRING']))
-			<span class="{{ $block->elem('countries') }}">{!! $arResult['COUNTRIES_STRING'] !!}</span>
-		@endif
-	</a>
+		<span class="{{ $block->elem('img') }} product-item-image-original" id="{{ $itemIds['PICT'] }}" style="background-image: url({{ $item['DETAIL_PICTURE']['SRC'] }})"></span>
+        <span class="{{ $block->elem('img') }} product-item-image-alternative" id="{{ $itemIds['SECOND_PICT'] }}" style="background-image: url({{ $item['DETAIL_PICTURE']['SRC'] }})"></span>
+    </a>
+	<div class="{{ $block->elem('name') }} product-item-title">
+	<a href="{!! $item['DETAIL_PAGE_URL'] !!}">{!! $item['NAME'] !!}</a></div>
+	@if (!empty($arResult['COUNTRIES_STRING']))
+		<span class="{{ $block->elem('countries') }}">{!! $arResult['COUNTRIES_STRING'] !!}</span>
+	@endif
 </div>
