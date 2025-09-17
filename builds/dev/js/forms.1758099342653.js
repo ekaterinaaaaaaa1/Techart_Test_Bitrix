5eead3973934c64b4e83ea6e40c3dc9462d65acd
ekaterinaaaaaaa1 <ekaterina_kuzmina_2002@mail.ru>
2025-09-17 12:19:24 +0300

    Сделана проверка на наличие телефона или почты в JS.

--- builds/dev/js/forms.1758099342653.js
+++ builds/dev/js/forms.1758099342653.js
@@ -81,18 +81,24 @@ document.querySelector('form').addEventListener('submit', function (form) {
   var requireds = document.getElementsByClassName('required');
   var messages = document.getElementsByClassName('b-forms-form__messages')[0];
   messages.innerHTML = '';
+  var emailOrPhone = false;
   for (const required of requireds) {
     switch (required.tagName.toLowerCase()) {
       case 'input':
         switch (required.type) {
           case 'text':
-            if (required.value.trim().length <= 1) {
+            if (required.value.trim().length < 1) {
               messages.innerHTML += '<span class="b-forms-form__message">Поле "Ваше Имя" не заполнено!</span>';
             }
             break;
           case 'email':
-            if (required.value.trim().length <= 1) {
-              messages.innerHTML += '<span class="b-forms-form__message">Поле "Ваш E-mail" не заполнено!</span>';
+            if (!(required.value.trim().length < 1)) {
+              emailOrPhone = true;
+            }
+            break;
+          case 'tel':
+            if (!(required.value.trim().length < 1)) {
+              emailOrPhone = true;
             }
             break;
           case 'checkbox':
@@ -103,11 +109,14 @@ document.querySelector('form').addEventListener('submit', function (form) {
         }
         break;
       case 'textarea':
-        if (required.value.trim().length <= 1) {
+        if (required.value.trim().length < 1) {
           messages.innerHTML += '<span class="b-forms-form__message">Поле "Сообщение" не заполнено!</span>';
         }
         break;
     }
+  }
+  if (!emailOrPhone) {
+    messages.innerHTML += '<span class="b-forms-form__message">Должно быть заполнено хотя бы одно из полей "Ваш E-mail" и "Ваш телефон"!</span>';
   }
   if (!(messages.innerHTML.length === 0)) {
     form.preventDefault();
@@ -278,4 +287,4 @@ __webpack_require__.r(__webpack_exports__);
 
 /******/ })()
 ;
-//# sourceMappingURL=forms.1758096043314.js.map
+//# sourceMappingURL=forms.1758099342653.js.map
--- frontend/src/block/forms/forms-form/forms-form.js
+++ frontend/src/block/forms/forms-form/forms-form.js
@@ -2,19 +2,25 @@ document.querySelector('form').addEventListener('submit', function(form) {
 	var requireds = document.getElementsByClassName('required');
 	var messages = document.getElementsByClassName('b-forms-form__messages')[0];
 	messages.innerHTML = '';
+	var emailOrPhone = false;
 
 	for (const required of requireds) {
 		switch (required.tagName.toLowerCase()) {
 			case 'input':
 				switch(required.type) {
 					case 'text':
-						if (required.value.trim().length <= 1) {
+						if (required.value.trim().length < 1) {
 							messages.innerHTML += '<span class="b-forms-form__message">Поле "Ваше Имя" не заполнено!</span>';
 						}
 						break;
 					case 'email':
-						if (required.value.trim().length <= 1) {
-							messages.innerHTML += '<span class="b-forms-form__message">Поле "Ваш E-mail" не заполнено!</span>';
+						if (!(required.value.trim().length < 1)) {
+							emailOrPhone = true;
+						}
+						break;
+					case 'tel':
+						if (!(required.value.trim().length < 1)) {
+							emailOrPhone = true;
 						}
 						break;
 					case 'checkbox':
@@ -25,11 +31,15 @@ document.querySelector('form').addEventListener('submit', function(form) {
 				}
 				break;
 			case 'textarea':
-				if (required.value.trim().length <= 1) {
+				if (required.value.trim().length < 1) {
 					messages.innerHTML += '<span class="b-forms-form__message">Поле "Сообщение" не заполнено!</span>';
 				}
 				break;
 		}
+	}
+
+	if (!emailOrPhone) {
+		messages.innerHTML += '<span class="b-forms-form__message">Должно быть заполнено хотя бы одно из полей "Ваш E-mail" и "Ваш телефон"!</span>';
 	}
 
 	if (!(messages.innerHTML.length === 0)) {
